9c61fda5ac648f1d9d5e6e1e0e0d3d9e03a3a8d4
София Занько <sofya_zanko@mail.ru>
2025-09-17 19:48:11 +0500

    Моя первая программа

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Привет мир!");
+            Console.ResetColor();
+
+
+            Console.ReadKey();
+        }
+    }
+}
