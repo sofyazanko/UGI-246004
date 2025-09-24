49c6085b524175fe3a87ebe09c61b61b01427c14
София Занько <sofya_zanko@mail.ru>
2025-09-24 20:49:39 +0500

    Задание 2 выполнено

---
+++ task02/task02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону прямоугольного параллелепипеда");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вторую сторону прямоугольного параллелепипеда");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третью сторону прямоугольного параллелепипеда");
+            var c = double.Parse(Console.ReadLine());
+
+            var area = 2 * (a * b + a * c + b * c);
+            var shape = a * b * c;
+
+            Console.WriteLine("Площадь поверхности прямоугольного параллелепипеда равна  " + area);
+            Console.WriteLine("Объем прямоугольного параллелепипеда равен  " + shape );
+        }
+    }
+}
