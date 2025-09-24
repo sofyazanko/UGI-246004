bf1e3fd4d7681f599895409e592d57599a6ac673
София Занько <sofya_zanko@mail.ru>
2025-09-24 19:53:57 +0500

    Задание 1 выполнено

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Михаил Юрьевич Лермонтов");
+
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine("Бородино");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Скажи-ка, дядя, ведь недаром Москва, спалённая пожаром,");
+            Console.WriteLine("Французу отдана? Ведь были ж схватки боевые,");
+            Console.WriteLine("Да, говорят, ещё какие! Недаром помнит вся Россия");
+            Console.WriteLine("Про день Бородина!");
+
+            Console.ResetColor();
+
+        }
+    }
+}
