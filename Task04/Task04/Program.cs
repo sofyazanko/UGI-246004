9a4d17c34c0b46319c46f135f1dc80adfef2badb
София Занько <sofya_zanko@mail.ru>
2025-10-01 20:50:39 +0500

    Задание выполнено

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            var x=double.Parse(Console.ReadLine());
+            var y=F(x);
+
+            Console.WriteLine("y=" + y);
+        }
+        static double F(double x)
+            {
+            return Math.Abs(x+(Math.Abs(x+(Math.Abs(x+Math.Sqrt(x+1))))));
+        }
+    }
+}
