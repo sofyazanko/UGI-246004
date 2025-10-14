425961ce5ac3e5b1ba5225e5e485b6c849e3c4a3
София Занько <sofya_zanko@mail.ru>
2025-10-14 22:28:07 +0500

    Задание

---
+++ Task06/Task06/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку на английском языке");
+            var text= Console.ReadLine();
+
+            Console.WriteLine("Вот что получилось:");
+            Console.WriteLine(Translate(text));
+        }
+        static string Translate(string s)
+        {
+            var result = s.ToUpper()
+                .Replace("A", "4")
+                .Replace("B", "8")
+                .Replace("C", "(")
+                .Replace("D", "|)")
+                .Replace("E", "3")
+                .Replace("F", "|=")
+                .Replace("G", "6")
+                .Replace("H", "|-|")
+                .Replace("I", "!")
+                .Replace("J", ")")
+                .Replace("K", "|<")
+                .Replace("L", "1")
+                .Replace("M", @"|\/|")
+                .Replace("N", @"|\|")
+                .Replace("O", "0")
+                .Replace("P", "|>")
+                .Replace("Q", "9")
+                .Replace("R", "|2")
+                .Replace("S", "5")
+                .Replace("T", "7")
+                .Replace("U", "|_|")
+                .Replace("V", @"\/")
+                .Replace("W", @"\/\/")
+                .Replace("X", "><")
+                .Replace("Y", "`/")
+                .Replace("Z", "2");
+
+            return result;
+        }
+    }
+}
---
+++ Task06/Task06part2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string main = "теплоход";
+
+            string ho = main.Substring(5, 2);
+            string lo = main.Substring(3, 2);
+            string d = main.Substring(7, 1);
+            string e = main.Substring(1, 1);
+
+
+            string cold = ho + lo + d;
+            string delo = d + e + lo;
+
+            Console.WriteLine("Дано слово:" + main);
+            Console.WriteLine("Новое слово получилось 'холод':" + cold);
+            Console.WriteLine("Новое слово получилось 'дело':" + delo);
+
+        }
+    }
+}
+
+
+
+
+
