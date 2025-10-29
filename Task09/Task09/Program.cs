7f84c73abff664d6d436a3177c2675b243a3336a
София Занько <sofya_zanko@mail.ru>
2025-10-29 18:41:47 +0500

    Задание 9 выполнено

---
+++ Task09/Task09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"f({x:F2})={F(x):F2}");
+        }
+        static double F(double x)
+        {
+            if (x > 1)
+                return Math.Log(x);
+            else if (x > 0 && x<=1)
+                return -Math.Log(x);
+            else
+                return 0;
+        }
+    }
+}
